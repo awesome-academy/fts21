042bb511d8c74768ff56cb0336da31f80c0098c6
Huu Hung <huuhung1996@gmail.com>
2019-03-04 16:17:59 +0700

    Finish Manage Task (CRUD Task with ajax)

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,8 @@ class Task < ApplicationRecord
   belongs_to :subject
   has_many :user_tasks, dependent: :destroy
 
+  validates :name, presence: true,
+    length: {minimum: Settings.task.name_min_length}
+
   scope :newest, ->{order created_at: :desc}
 end
