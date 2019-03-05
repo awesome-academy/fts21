21422e35c0aacdd340640794b99da0de9d7b62f9
Huu Hung <huuhung1996@gmail.com>
2019-03-05 14:49:00 +0700

    Finish Trainee view course

---
+++ app/controllers/trainee/user_courses_controller.rb
@@ -0,0 +1,21 @@
+class Trainee::UserCoursesController < TraineeController
+  before_action :load_user_course, only: :show
+  before_action :correct_user
+
+  def show
+    @course = @user_course.course
+    @course_subjects = @course.course_subjects.includes :subject
+    @trainees = @user_course.course.trainees.paginate page: params[:page],
+      per_page: Settings.pagination.per_page
+  end
+
+  private
+  def load_user_course
+    @user_course = UserCourse.find_by id: params[:id]
+    @user_course || render(file: "public/404.html", status: 404, layout: true)
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user_course.user
+  end
+end
