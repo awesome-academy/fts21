65173e2b4002e68dbcb45d1a478cf710252d719c
Huu Hung <huuhung1996@gmail.com>
2019-03-10 20:56:08 +0700

    Finish View course progress of trainee

--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -1,8 +1,13 @@
 class UserSubject < ApplicationRecord
   belongs_to :user
   belongs_to :course_subject
-  has_many :user_tasks
+  has_many :user_tasks, dependent: :destroy
 
   enum status: {joined: 0, active: 1, finished: 2}
   scope :by_user, ->(user_id){where user_id: user_id}
+  scope :task_by_course, lambda{|course_id, user_id|
+    joins(:course_subject, :user_tasks)
+      .where "course_subjects.course_id": course_id,
+      user_id: user_id, "user_tasks.status": :finished
+  }
 end
