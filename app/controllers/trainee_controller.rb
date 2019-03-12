90213ad2c0ce17950e923141d9c684a02c6b06dd
Huu Hung <huuhung1996@gmail.com>
2019-03-12 12:59:25 +0700

    Replace authentication with devise gem

--- app/controllers/trainee_controller.rb
+++ app/controllers/trainee_controller.rb
@@ -1,3 +1,3 @@
 class TraineeController < ApplicationController
-  before_action :logged_in_user
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "activities#index"
 
   get "/login", to: "sessions#new"
